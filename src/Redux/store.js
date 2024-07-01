68e75857612dadcdb609392bab67726444ebbee3
Beast0E4 <ebison.04@gmail.com>
2024-07-01 18:51:02 +0530

    Added auth slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import authSLiceReducer from "./Slices/AuthSLice";
 
 const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authSLiceReducer
+    },
     devTools: true
 });
 
