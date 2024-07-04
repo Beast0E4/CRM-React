393cad95e6a7d892448fdeb62262dd73d625e2b4
Beast0E4 <ebison.04@gmail.com>
2024-07-04 12:54:03 +0530

    Feat: Completed status cards

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,7 +1,7 @@
-function Card({ children, fontColor = "text-white", dividerColor = "bg-gray-100", borderColor = "border-secondary", backgorund = "bg-primary", status = 50, titleText = "Card", quantity = 50 }) {
+function Card({ children, fontColor = "text-black", dividerColor = "bg-black", borderColor = "border-blue-500", backgorund = "bg-blue-300", status = 50, titleText = "Card", quantity = 50 }) {
     return (
-        <div className={`border-b-8 ${borderColor} w-64 h-44 rounded-md ${backgorund} flex flex-col justify-center items-center py-2`}>
-            <div className="text-black text-2xl mb-2">
+        <div className={`hover:scale-105 hover:transition-all ease-in-out duration-300 border-b-8 ${borderColor} w-64 h-44 rounded-md ${backgorund} flex flex-col justify-center items-center py-2`}>
+            <div className="text-black text-2xl mb-1 mt-1">
                 {children} <span>{titleText}</span>
             </div>
             <div className={`divider h-0.5 mx-4 rounded-sm ${dividerColor}`}></div>
--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 import { FaPencilAlt } from "react-icons/fa";
+import { GrInProgress } from "react-icons/gr";
+import { IoMdDoneAll } from "react-icons/io";
+import { MdPending } from "react-icons/md";
+import { TiCancel } from "react-icons/ti";
 import { useDispatch, useSelector } from "react-redux";
 
 import Card from "../../components/card";
@@ -24,9 +28,51 @@ function Home() {
 
     return (
         <HomeLayout>
-            <Card>
-                <FaPencilAlt className="inline mr-2"/>
-            </Card>
+            <div className="flex flex-row gap-5 justify-center items-center flex-wrap mt-10">
+                <Card 
+                    titleText="Open"
+                    status={ticketState.ticketList.length ? (ticketState.ticketDistribution.open / ticketState.ticketList.length * 100).toFixed(2) : 0}
+                    quantity={ticketState.ticketDistribution.open}
+                >
+                    <FaPencilAlt className="inline mr-2"/>
+                </Card>
+                <Card 
+                    titleText="In Progress"
+                    status={ticketState.ticketList.length ? (ticketState.ticketDistribution.inProgress / ticketState.ticketList.length * 100).toFixed(2) : 0}
+                    quantity={ticketState.ticketDistribution.inProgress}
+                    backgorund="bg-orange-300"
+                    borderColor="border-orange-400"
+                >
+                    <GrInProgress className="inline mr-2"/>
+                </Card>
+                <Card 
+                    titleText="Resolved"
+                    status={ticketState.ticketList.length ? (ticketState.ticketDistribution.resolved / ticketState.ticketList.length * 100).toFixed(2) : 0}
+                    quantity={ticketState.ticketDistribution.resolved}
+                    backgorund="bg-green-300"
+                    borderColor="border-green-500"
+                >
+                    <IoMdDoneAll className="inline mr-2"/>
+                </Card>
+                <Card 
+                    titleText="On Hold"
+                    status={ticketState.ticketList.length ? (ticketState.ticketDistribution.onHold / ticketState.ticketList.length * 100).toFixed(2) : 0}
+                    quantity={ticketState.ticketDistribution.onHold}
+                    backgorund="bg-accent"
+                    borderColor="border-red-500"
+                >
+                    <MdPending className="inline mr-2"/>
+                </Card>
+                <Card 
+                    titleText="Cancelled"
+                    status={ticketState.ticketList.length ? (ticketState.ticketDistribution.cancelled / ticketState.ticketList.length * 100).toFixed(2) : 0}
+                    quantity={ticketState.ticketDistribution.cancelled}
+                    backgorund="bg-red-300"
+                    borderColor="border-red-400"
+                >
+                    <TiCancel className="inline mr-2"/>
+                </Card>
+            </div>
         </HomeLayout>
     );
 }
