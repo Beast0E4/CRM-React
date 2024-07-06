c4b0ba82dad45bee31991ecddc053cdd0c896698
Beast0E4 <ebison.04@gmail.com>
2024-07-06 23:27:02 +0530

    Feat: Added generic user update handler

--- src/components/UserDetails.modal.jsx
+++ src/components/UserDetails.modal.jsx
@@ -7,16 +7,18 @@ function UserDetailsModal ({ user, resetTable }) {
 
     const [userDisplay, setUserDisplay] = useState(user);
 
-    async function handleStatusChange(e) {
+    async function handleUserChange(e) {
         try {
-            const dropdown = document.getElementById('userStatus-dropdown');
+            const ul = e.target.parentNode.parentNode;
+            const name = ul.getAttribute("name");
+            const dropdown = document.getElementById(`${name}Dropdown`);
             dropdown.open = !dropdown.open;
             toast(`Updating the user ...`);
             const response = await axiosInstance.patch("user/updateUser", {
                 userId: userDisplay.id,
                 updates: {
                     ...userDisplay,
-                    userStatus: e.target.textContent.toLowerCase()
+                    [name]: e.target.textContent.toLowerCase()
                 }
             },{
                 headers: {
@@ -27,11 +29,12 @@ function UserDetailsModal ({ user, resetTable }) {
                 toast.success(`Successfully updated the user`);
                 const user = response?.data?.result;
                 setUserDisplay({
+                    ...userDisplay,
                     name: user.name,
                     email: user.email,
                     userStatus: user.userStatus,
                     userType: user.userType,
-                    clientName: user.clientName
+                    clientName: user.clientName,
                 });
                 resetTable();
             }
@@ -46,17 +49,26 @@ function UserDetailsModal ({ user, resetTable }) {
                 <h3 className="font-bold text-lg text-yellow-600">USER DETAILS</h3>
                 <p className="py-2"><span className="font-bold text-yellow-600 text-lg">Name : </span> {userDisplay.name}</p>
                 <p className="py-2"><span className="font-bold text-yellow-600 text-lg">Email : </span> {userDisplay.email}</p>
-                <span className="py-2"><span className="font-bold text-yellow-600 text-lg">Status : </span> 
-                    <details className="dropdown py-2" id="userStatus-dropdown">
+                <div className="py-2"><span className="font-bold text-yellow-600 text-lg">Status : </span> 
+                    <details className="dropdown py-2" id="userStatusDropdown">
                         <summary className="btn m-1 uppercase">{userDisplay.userStatus}</summary>
-                        <ul onClick={handleStatusChange} className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                        <ul name="userStatus" onClick={handleUserChange} className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                             <li><a>Approved</a></li>
                             <li><a>Suspended</a></li>
                             <li><a>Rejected</a></li>
                         </ul>
                     </details>
-                </span>
-                <p className="py-2"><span className="font-bold text-yellow-600 text-lg">Type : </span> {userDisplay.userType}</p>
+                </div>
+                <div className="py-2"><span className="font-bold text-yellow-600 text-lg">Type : </span> 
+                    <details className="dropdown py-2" id="userTypeDropdown">
+                        <summary className="btn m-1 uppercase">{userDisplay.userType}</summary>
+                        <ul name="userType" onClick={handleUserChange} className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                            <li><a>Engineer</a></li>
+                            <li><a>Customer</a></li>
+                            <li><a>Admin</a></li>
+                        </ul>
+                    </details>
+                </div>
                 <p className="py-2"><span className="font-bold text-yellow-600 text-lg">Client Name : </span> {userDisplay.clientName}</p>
             </div>
             <form method="dialog" className="modal-backdrop">
